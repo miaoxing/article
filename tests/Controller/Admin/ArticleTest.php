656dcbac2cd89216ab5c81d00683da0cda5f5a16
twinh <twinhuang@qq.com>
2018-02-20 00:34:44 +0800

    单元测试创建图文流程 #3251

--- tests/Controller/Admin/ArticleTest.php
+++ tests/Controller/Admin/ArticleTest.php
@@ -8,7 +8,48 @@ class ArticleTest extends BaseControllerTestCase
 {
     public function testCreate()
     {
-        $category = wei()->category();
+        $category = wei()->category()->save([
+            'parentId' => 'article',
+            'type' => 'article',
+            'name' => '测试分类名称',
+        ]);
+
+        $ret = wei()->tester()
+            ->login(1)
+            ->controller('admin/article')
+            ->action('create')
+            ->request([
+                'categoryId' => $category['id'],
+                'title' => '测试图文标题',
+                'author' => '作者',
+                'thumb' => '缩略图',
+                'intro' => '摘要',
+                'content' => '内容',
+                'sort' => 100,
+            ])
+            ->json()
+            ->response();
+        $this->assertRetSuc($ret);
+
+        $ret = wei()->tester()
+            ->login(1)
+            ->controller('admin/article')
+            ->json()
+            ->response();
+        $this->assertRetSuc($ret);
+
+        $this->assertArraySubset([
+            'categoryId' => $category['id'],
+            'title' => '测试图文标题',
+            'author' => '作者',
+            'thumb' => '缩略图',
+            'intro' => '摘要',
+            'content' => '内容',
+            'sort' => 100,
+            'category' => [
+                'name' => '测试分类名称',
+            ],
+        ], $ret['data'][0]);
     }
 
     public function testDestroy()
