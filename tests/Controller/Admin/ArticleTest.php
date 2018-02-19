3c820945ee3f32ea007af6dc5480e19d09c2ec7f
twinh <twinhuang@qq.com>
2018-02-19 23:48:42 +0800

    单元测试删除图文流程 #3252

--- tests/Controller/Admin/ArticleTest.php
+++ tests/Controller/Admin/ArticleTest.php
@@ -6,4 +6,25 @@ use Miaoxing\Plugin\Test\BaseControllerTestCase;
 
 class ArticleTest extends BaseControllerTestCase
 {
+    public function testDestroy()
+    {
+        $article = wei()->article()->save();
+        $ret = wei()->tester()
+            ->login(1)
+            ->controller('admin/article')
+            ->action('show')
+            ->request(['id' => $article['id']])
+            ->json()
+            ->response();
+        $this->assertRetSuc($ret);
+
+        $article->destroy();
+        $this->setExpectedException(\Exception::class, 'Record not found', 404);
+        wei()->tester()
+            ->login(1)
+            ->controller('admin/article')
+            ->action('show')
+            ->request(['id' => $article['id']])
+            ->response();
+    }
 }
