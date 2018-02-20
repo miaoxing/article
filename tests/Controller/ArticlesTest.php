852929e21831b0b99e5ae320817f33dbea5780b6
twinh <twinhuang@qq.com>
2018-02-20 15:18:55 +0800

    单元测试访问带分类的文章列表 closes #3198

--- tests/Controller/ArticlesTest.php
+++ tests/Controller/ArticlesTest.php
@@ -6,6 +6,36 @@ use Miaoxing\Plugin\Test\BaseControllerTestCase;
 
 class ArticlesTest extends BaseControllerTestCase
 {
+    public function testIndex()
+    {
+        $category = wei()->category()->save([
+            'parentId' => 'article',
+            'type' => 'article',
+            'name' => '测试分类名称',
+        ]);
+
+        $article = wei()->article()->save([
+            'categoryId' => $category['id'],
+            'title' => '测试图文标题',
+            'author' => '作者',
+            'thumb' => '缩略图',
+            'intro' => '摘要',
+            'content' => '内容',
+            'sort' => 100,
+        ]);
+
+        $res = wei()->tester()
+            ->controller('articles')
+            ->request(['categoryId' => $category['id']])
+            ->response();
+
+        $this->assertContains($category['name'], $res);
+        $this->assertContains($article['title'], $res);
+
+        $article = wei()->article()->findOneById($article['id']);
+        $this->assertContains($article->getUrl(), $res);
+    }
+
     public function testCreate()
     {
         wei()->setting->setValue('site.title', '测试站点');
