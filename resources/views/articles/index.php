5d43b58c042c7a0705ff3c549a9fe5910b3b1949
twinh <twinhuang@qq.com>
2018-11-03 23:59:01 +0800

    feat: 增加图文的轮播图

--- resources/views/articles/index.php
+++ resources/views/articles/index.php
@@ -4,4 +4,6 @@ $view->layout();
 $wei->page->addAsset('plugins/article/css/articles.css')
   ->addCss('//at.alicdn.com/t/font_872953_843lwiv79j5.css');
 
+$wei->event->trigger('beforeArticlesIndexRender');
+
 require $app->getControllerFile('lists/' . $tpl);
