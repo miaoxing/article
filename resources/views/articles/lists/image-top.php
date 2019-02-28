e8cc6dd0802fbaec65e95b5b83a254a6709d4f76
twinh <twinhuang@qq.com>
2019-02-28 10:55:32 +0800

    refactoring: 去掉list-row,父类可使用 flex-wrap 或 flex-column 实现

--- resources/views/articles/lists/image-top.php
+++ resources/views/articles/lists/image-top.php
@@ -1,18 +1,14 @@
 <ul class="list article-list">
   <?php foreach ($articles as $article) : ?>
     <li>
-      <a class="list-item" href="<?= $article->getUrl() ?>">
-        <div class="list-row article-image-top">
-          <img src="<?= wei()->asset->thumb($article['thumb'], 750) ?>">
-        </div>
-        <div class="list-row">
-          <h4 class="list-heading">
-            <?= $article['title'] ?>
-          </h4>
-          <div class="list-body">
-            <?= $article['intro'] ?>
-            <?php require $view->getFile('@article/articles/_like.php') ?>
-          </div>
+      <a class="list-item flex-column" href="<?= $article->getUrl() ?>">
+        <img class="img-fluid mb-2" src="<?= wei()->asset->thumb($article['thumb'], 750) ?>">
+        <h4 class="list-heading">
+          <?= $article['title'] ?>
+        </h4>
+        <div class="list-body">
+          <?= $article['intro'] ?>
+          <?php require $view->getFile('@article/articles/_like.php') ?>
         </div>
       </a>
     </li>
