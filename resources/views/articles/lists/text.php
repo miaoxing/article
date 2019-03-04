78232dbfaf7245715dec7f2ef9548672e2eae6ff
twinh <twinhuang@qq.com>
2019-03-04 13:15:19 +0800

    refactoring: .list-feedback > .list-arrow

--- resources/views/articles/lists/text.php
+++ resources/views/articles/lists/text.php
@@ -1,11 +1,11 @@
 <ul class="list">
   <?php foreach ($articles as $article) : ?>
     <li>
-      <a href="<?= $article->getUrl() ?>" class="list-item list-has-feedback">
+      <a href="<?= $article->getUrl() ?>" class="list-item list-has-arrow">
         <h4 class="list-title">
           <?= $article['title'] ?>
         </h4>
-        <i class="bm-angle-right list-feedback"></i>
+        <i class="bm-angle-right list-arrow"></i>
       </a>
     </li>
   <?php endforeach ?>
