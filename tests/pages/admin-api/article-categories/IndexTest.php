21b6d588d77c73ec509b7b0d7e337877728d7326
twinh <twinhuang@qq.com>
2021-05-16 16:44:50 +0800

    test(article-category): 增加查看图文分类列表后端测试

--- tests/pages/admin-api/article-categories/IndexTest.php
+++ tests/pages/admin-api/article-categories/IndexTest.php
@@ -1,28 +1,55 @@
 <?php
 
-namespace MiaoxingTest\Category\Pages\AdminApi\Categories;
+namespace MiaoxingTest\Article\Pages\AdminApi\ArticleCategories;
 
-use Miaoxing\Category\Service\CategoryModel;
+use Miaoxing\Article\Service\ArticleCategoryModel;
 use Miaoxing\Plugin\Service\Tester;
 use Miaoxing\Plugin\Service\User;
 use Miaoxing\Plugin\Test\BaseTestCase;
 
 class IndexTest extends BaseTestCase
 {
+    public function testGet()
+    {
+        User::loginById(1);
+
+        $category = ArticleCategoryModel::save([
+            'name' => '测试',
+            'sort' => 30,
+            'description' => '测试描述',
+        ]);
+        ArticleCategoryModel::save([
+            'parentId' => $category->id,
+            'name' => '子分类',
+            'sort' => 60,
+            'description' => '子分类描述',
+            'level' => 2,
+        ]);
+
+        $ret = Tester::request(['search' => ['id' => $category->id]])->getAdminApi('article-categories');
+
+        $categories = ArticleCategoryModel::findAll([$category->id])
+            ->load('children')
+            ->toArray();
+
+        $this->assertSame($categories, $ret['data'], '返回列表数据包含下级');
+    }
+
     public function testPost()
     {
         User::loginById(1);
 
-        $ret = Tester::postAdminApi('categories', [
+        $ret = Tester::postAdminApi('article-categories', [
             'id' => '1', // ignored
             'level' => '2', // ignored
+            'parentId' => 0,
             'name' => '测试',
             'sort' => '60',
             'description' => '描述',
         ]);
         $this->assertRetSuc($ret);
 
-        /** @var CategoryModel $category */
+        /** @var ArticleCategoryModel $category */
         $category = $ret['data'];
         $this->assertNotEquals('1', $category->id);
         $this->assertSame('测试', $category->name);
@@ -35,14 +62,15 @@ class IndexTest extends BaseTestCase
     {
         User::loginById(1);
 
-        $category = CategoryModel::save();
+        $category = ArticleCategoryModel::save();
 
-        $ret = Tester::postAdminApi('categories', [
+        $ret = Tester::postAdminApi('article-categories', [
             'parentId' => $category->id,
+            'name' => '测试分类',
         ]);
         $this->assertRetSuc($ret);
 
-        /** @var CategoryModel $subCategory */
+        /** @var ArticleCategoryModel $subCategory */
         $subCategory = $ret['data'];
         $this->assertSame($category->id, $subCategory->parentId);
         $this->assertSame(2, $subCategory->level);
