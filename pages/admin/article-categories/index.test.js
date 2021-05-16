11c4fa86d27145a8b667b2c947b17de87ebbea94
twinh <twinhuang@qq.com>
2021-05-16 16:47:00 +0800

    test(article-category): 增加查看图文分类列表前端测试

---
+++ pages/admin/article-categories/index.test.js
@@ -0,0 +1,61 @@
+import Index from './index';
+import {render} from '@testing-library/react';
+import {MemoryRouter} from 'react-router';
+import React from 'react';
+import $ from 'miaoxing';
+import {bootstrap, createPromise, setUrl, resetUrl} from '@mxjs/test';
+import {app} from '@mxjs/app';
+
+bootstrap();
+
+describe('admin/article-categories', () => {
+  beforeEach(function () {
+    setUrl('admin/article-categories');
+    app.page = {
+      collection: 'admin/article-categories',
+      index: true,
+    };
+  });
+
+  afterEach(() => {
+    resetUrl();
+    app.page = {};
+  });
+
+  test('index', async () => {
+    const promise = createPromise();
+
+    $.http = jest.fn()
+      // 读取列表数据
+      .mockImplementationOnce(() => promise.resolve({
+        code: 1,
+        data: [
+          {
+            id: 1,
+            parentId: 0,
+            sort: 50,
+            name: '测试分类',
+            children: [
+              {
+                id: 2,
+                parentId: 1,
+                sort: 50,
+                name: '子分类',
+              },
+            ],
+          },
+        ],
+      }));
+
+    const {findByText} = render(<MemoryRouter>
+      <Index/>
+    </MemoryRouter>);
+
+    await findByText('测试分类');
+    await findByText('└ 子分类');
+
+    await Promise.all([promise]);
+    expect($.http).toHaveBeenCalledTimes(1);
+    expect($.http).toMatchSnapshot();
+  });
+});
