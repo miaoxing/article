42df928fef4099a7549f5e430d51ea652d5a910d
twinh <twinhuang@qq.com>
2021-05-17 18:54:58 +0800

    refactor(article): 页面配置 `expand` 来加载关联模型

--- pages/admin-api/article-categories/index.php
+++ pages/admin-api/article-categories/index.php
@@ -2,11 +2,17 @@
 
 use Miaoxing\Article\Service\ArticleCategoryModel;
 use Miaoxing\Plugin\BaseController;
+use Miaoxing\Services\Page\CollGetTrait;
 use Miaoxing\Services\Page\PostToPatchTrait;
 use Miaoxing\Services\Service\IndexAction;
 
 return new class extends BaseController {
+    use CollGetTrait;
     use PostToPatchTrait;
+
+    protected $expand = [
+        'children',
+    ];
 
     public function get()
     {
@@ -14,9 +20,6 @@ return new class extends BaseController {
             ::beforeFind(function (ArticleCategoryModel $models) {
                 $models->where('level', 1)
                     ->setDefaultSortColumn(['sort', 'id']);
-            })
-            ->afterFind(function (ArticleCategoryModel $models) {
-                $models->load('children');
             })
             ->exec($this);
     }
--- pages/admin-api/articles/index.php
+++ pages/admin-api/articles/index.php
@@ -2,20 +2,23 @@
 
 use Miaoxing\Article\Service\ArticleModel;
 use Miaoxing\Plugin\BaseController;
+use Miaoxing\Services\Page\CollGetTrait;
 use Miaoxing\Services\Page\PostToPatchTrait;
 use Miaoxing\Services\Service\IndexAction;
 
 return new class extends BaseController {
+    use CollGetTrait;
     use PostToPatchTrait;
+
+    protected $expand = [
+        'category',
+    ];
 
     public function get()
     {
         return IndexAction
             ::beforeFind(function (ArticleModel $models) {
                 $models->setDefaultSortColumn(['sort', 'id']);
-            })
-            ->afterFind(function (ArticleModel $models) {
-                $models->load('category');
             })
             ->exec($this);
     }
