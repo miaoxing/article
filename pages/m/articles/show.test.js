9911edd167bda4828a57ab915055ab93a5a49f73
twinh <twinhuang@qq.com>
2021-07-07 23:36:38 +0800

    test(article): 增加小程序查看图文详情页面

---
+++ pages/m/articles/show.test.js
@@ -0,0 +1,46 @@
+import Show from './show';
+import {render, waitFor} from '@testing-library/react';
+import $, {Ret} from 'miaoxing';
+import {createPromise, bootstrap, setUrl, resetUrl} from '@mxjs/test';
+import Taro from '@tarojs/taro';
+
+bootstrap();
+
+describe('Show', () => {
+  beforeEach(() => {
+    setUrl('/articles/show?id=1');
+  });
+
+  afterEach(() => {
+    resetUrl();
+  });
+
+  test('basic', async () => {
+    const promise = createPromise();
+
+    $.http = jest.fn()
+      .mockImplementationOnce(() => promise.resolve({
+        ret: Ret.new({
+          code: 0,
+          data: {
+            title: 'title',
+            updatedAt: '2021-01-01 01:01:01',
+            detail: {
+              content: 'content',
+            },
+          },
+        }),
+      }));
+
+    Taro.setBackgroundColor = jest.fn();
+
+    const {container, getByText} = render(<Show/>);
+
+    await waitFor(() => {
+      expect(getByText('content')).not.toBeNull();
+    });
+
+    expect(container).toMatchSnapshot();
+    expect($.http).toMatchSnapshot();
+  });
+});
